120cb7414e2ae9e2e9b4576890792507409ded95
Mistral Contrastin <me@madgen.net>
2018-08-23 10:40:58 +0100

    Make vector a functor

--- src/Util/Vector.hs
+++ src/Util/Vector.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Util.Vector
   ( module Util.Nat
@@ -8,12 +10,18 @@ module Util.Vector
   , toList
   ) where
 
+import Protolude hiding (Nat, toList)
+
 import Util.Nat
 
 infixr 5 :::
 data Vector :: Nat -> * -> * where
   Nil   :: Vector 'Zero a
   (:::) :: a -> Vector n a -> Vector ('Succ n) a
+
+deriving instance Eq a => Eq (Vector n a)
+deriving instance Ord a => Ord (Vector n a)
+deriving instance Functor (Vector n)
 
 toList :: Vector n a -> [ a ]
 toList Nil = []
