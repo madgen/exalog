f223e8806078849ac7f188917b4750dc25e6d111
Mistral Contrastin <me@madgen.net>
2018-08-31 17:17:36 +0100

    General solver

---
+++ src/Language/Exalog/Solver.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Language.Exalog.Solver
+  ( solve
+  ) where
+
+import Protolude
+
+import           Language.Exalog.Core
+import           Language.Exalog.Stratification (stratify)
+import           Language.Exalog.SemiNaive (semiNaive)
+import qualified Language.Exalog.Relation as R
+
+solve :: Eq (PredicateAnn a) => Program a -> R.Solution a -> IO (R.Solution a)
+solve pr edb =
+  case eprs of
+    Left msg -> panic msg
+    Right prs -> foldrM (flip semiNaive) edb prs
+  where
+  eprs = stratify . decorate $ pr
