e89f1b10e4bb4d122d2e30d1ac0760c3f11883e8
Mistral Contrastin <me@madgen.net>
2018-08-31 16:44:27 +0100

    Add ltieral negating test utility

--- test/Fixture/Util.hs
+++ test/Fixture/Util.hs
@@ -4,13 +4,18 @@ module Fixture.Util
   ( tvar
   , tsym
   , lit
+  , not
   ) where
 
-import Protolude hiding (pred)
+import Protolude hiding (pred, not)
 
 import qualified Data.Vector.Sized as V
 
 import Language.Exalog.Core
+
+not :: Literal 'ABase -> Literal 'ABase
+not l@Literal{polarity = pol} =
+  l { polarity = if pol == Positive then Negative else Positive }
 
 lit :: Predicate n 'ABase -> V.Vector n Term -> Literal 'ABase
 lit = Literal LitABase Positive
