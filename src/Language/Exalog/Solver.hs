dd94815f7b07004ce441792eaf8bbd0dbc9ac154
Mistral Contrastin <me@madgen.net>
2018-09-07 14:18:15 +0100

    Execute stratified programs in corrext order

--- src/Language/Exalog/Solver.hs
+++ src/Language/Exalog/Solver.hs
@@ -11,10 +11,10 @@ import           Language.Exalog.Stratification (stratify)
 import           Language.Exalog.SemiNaive (semiNaive)
 import qualified Language.Exalog.Relation as R
 
-solve :: (Eq (PredicateAnn a), Show (PredicateAnn a), Show (ClauseAnn a), Show (LiteralAnn a)) => Program a -> R.Solution a -> IO (R.Solution a)
+solve :: (Eq (PredicateAnn a), Show (ProgramAnn a), Show (PredicateAnn a), Show (ClauseAnn a), Show (LiteralAnn a)) => Program a -> R.Solution a -> IO (R.Solution a)
 solve pr edb =
   case eprs of
     Left msg -> panic msg
-    Right prs -> foldrM (flip semiNaive) edb prs
+    Right prs -> foldrM (flip semiNaive) edb (reverse prs)
   where
   eprs = stratify . decorate $ pr
