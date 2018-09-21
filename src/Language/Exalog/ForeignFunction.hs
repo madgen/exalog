532efbd26e25eb1586d2d8ca78e10e84efc323dd
Mistral Contrastin <me@madgen.net>
2018-09-21 16:30:19 +0100

    Allow list and multiple return values for external functions

--- src/Language/Exalog/ForeignFunction.hs
+++ src/Language/Exalog/ForeignFunction.hs
@@ -1,8 +1,14 @@
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise -fplugin-opt GHC.TypeLits.Normalise:assert-constants-natural #-}
+
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Language.Exalog.ForeignFunction
@@ -12,15 +18,15 @@ module Language.Exalog.ForeignFunction
   , liftFunctionME
   ) where
 
-import Protolude
+import Protolude hiding (TypeError)
 
-import           Data.Type.Bool (type If)
+import           Data.Type.Bool (If)
 import           Data.Text (unpack, pack)
 import qualified Data.Vector.Sized as V
 
 import Text.Read (read)
 
-import GHC.TypeLits (type (+))
+import GHC.TypeLits as TL (type (+), type (<=))
 
 import Language.Exalog.Core
 
@@ -42,9 +48,7 @@ import Language.Exalog.Core
 - answer set. Otherwise, it returns an empty answer set.
 -}
 liftPredicate :: (Applicable f, RetTy f ~ Bool) => f -> ForeignFunc (Arity f)
-liftPredicate p v = return . return $ [ symbols | p @@ symbols ]
-  where
-  symbols = fromTerm <$> v
+liftPredicate p v = return . return $ [ fromTerm <$> v | p @@ v ]
 
 {- | A variant of 'liftPredicate' for functions that have side effects and
 - may produce errors.
@@ -52,56 +56,91 @@ liftPredicate p v = return . return $ [ symbols | p @@ symbols ]
 liftPredicateME :: (Applicable f, RetTy f ~ IO (Either Text Bool))
                 => f -> ForeignFunc (Arity f)
 liftPredicateME p v =
-  fmap (\cond -> [ symbols | cond ]) <$> p @@ symbols
-  where
-  symbols = fromTerm <$> v
+  fmap (\cond -> [ fromTerm <$> v | cond ]) <$> p @@ v
 
 --------------------------------------------------------------------------------
 -- Lift functions that do not return Bool
 --------------------------------------------------------------------------------
 
+data BaseTy = TString [ Char ] | TInt Int | TChar Char
+
 {- | Lifts Haskell functions to 'ForeignFunc' to back extralogical predicates.
 -
 - For example, if an extralogical predicate @p@ is backed by a Haskell
-- function of type @'Int' -> 'Char' -> 'String'@, a subgoal involving @p@
-- would be of the form @p(S,I,C)@, where @S@, @I@ and @C@ are 'Text'
-- representations of inhabitants of 'String', 'Int' and 'Char respectively.
+- function of type @'Int' -> 'Char' -> [ 'Int' ]@, a subgoal involving @p@
+- would be of the form @p(I1,C,I2)@, where @I1@, @C@, @I2@ are 'Text'
+- representations of inhabitants of 'Int', 'Char' and 'Int' respectively.
 -
-- While evaluating @p(S,I,C)@, the variables @I@ and @C@ have to be bound or are
-- constants, or it is an error. Variable @S@ may or may not be bound or
-- ground. This subgoal holds when the Haskell function backing it returns
-- a value of type 'String' which and if @S@ happens to be bound, this value
-- is compared to the value of @S@. Otherwise, it returns an empty answer set.
+- While evaluating @p(I1,C,I2)@, the variables @I1@ and @C@ have to be
+- bound or are constants, or it is an error. Variable @I2@ may or may not
+- be bound or ground. This subgoal holds when the Haskell function backing
+- it returns a value of type @[ 'Int' ]@ and if @IS@ happens to be bound,
+- its value needs to be in the returned list of integers. Otherwise, it
+- returns an empty answer set.
 -}
-liftFunction :: (Applicable f, RetTy f ~ b, Show b, Read b, Eq b)
-             => f -> ForeignFunc (1 + Arity f)
-liftFunction f v = return . return $
-  [ V.cons (toSym res) tailSymbols | res `consistent` V.head v ]
-  where
-  res = f @@ tailSymbols
-  tailSymbols = fromTerm <$> V.tail v
+liftFunction :: (Applicable f, RetTy f ~ r, Returnable r, KnownNat (Arity f))
+             => f -> ForeignFunc (NRets r + Arity f)
+liftFunction f v = return . return $ q (toBaseVector $ f @@ v) v
 
 {- | A variant of 'liftFunction' for functions that have side effects and
 - may produce errors.
 -}
-liftFunctionME :: (Applicable f, RetTy f ~ IO (Either Text b), Show b, Read b, Eq b)
-               => f -> ForeignFunc (1 + Arity f)
+liftFunctionME :: (Applicable f, RetTy f ~ IO (Either Text r), Returnable r, KnownNat (Arity f))
+               => f -> ForeignFunc (NRets r + Arity f)
 liftFunctionME f v = do
-  eRes <- f @@ tailSymbols
+  eResss <- fmap toBaseVector <$> f @@ v
   return $ do
-    res <- eRes
-    return [ V.cons (toSym res) tailSymbols | res `consistent` V.head v ]
-  where
-  tailSymbols = fromTerm <$> V.tail v
+    resss <- eResss
+    return $ q resss v
 
-consistent :: (Eq a, Read a) => a -> Term -> Bool
+q :: forall na nr
+   . KnownNat na
+  => [ V.Vector nr BaseTy ]
+  -> V.Vector (na + nr) Term
+  -> [ V.Vector (na + nr) Sym ]
+q resss v =
+  [ (fromTerm <$> args) V.++ (toSym <$> ress)
+  | ress <- resss
+  , V.all (uncurry consistent) $ V.zip ress rets ]
+  where
+  (args, rets) = V.splitAt @na v
+
+consistent :: BaseTy -> Term -> Bool
 consistent a = \case
-  TSym (Sym text) -> a == read (unpack text)
+  TSym (Sym text) ->
+    case a of
+      TInt i -> i == read (unpack text)
+      TChar c -> c == read (unpack text)
+      TString str -> str == read (unpack text)
   TVar{}  -> True
 
 --------------------------------------------------------------------------------
 -- Util
 --------------------------------------------------------------------------------
+
+type family NRets a :: Nat where
+  NRets Int = 1
+  NRets Char = 1
+  NRets [ a ] = NRets a
+  NRets (a,b) = NRets a + NRets b
+
+class Returnable r where
+  toBaseVector :: r -> [ V.Vector (NRets r) BaseTy ]
+
+instance Returnable Int where
+  toBaseVector i = [ V.singleton (TInt i) ]
+
+instance Returnable Char where
+  toBaseVector c = [ V.singleton (TChar c) ]
+
+instance {-# OVERLAPPING #-} Returnable a => Returnable [ Char ] where
+  toBaseVector str = [ V.singleton (TString str) ]
+
+instance {-# OVERLAPPABLE #-} Returnable a => Returnable [ a ] where
+  toBaseVector xs = join . map toBaseVector $ xs
+
+instance (Returnable a, Returnable b) => Returnable (a,b) where
+  toBaseVector (a, b) = zipWith (V.++) (toBaseVector a) (toBaseVector b)
 
 type family Ground a where
   Ground Bool   = 'True
@@ -117,24 +156,24 @@ type family Arity f :: Nat where
   Arity r = 1
 
 class Applicable f where
-  (@@) :: f -> V.Vector (Arity f) Sym -> RetTy f
+  (@@) :: Arity f <= n => f -> V.Vector n Term -> RetTy f
 
 instance ( Ground r ~ 'True
          , Read a
          ) => Applicable (a -> r) where
-  f @@ v | [ arg ] <- symToStr <$> V.toList v = f (read arg)
+  f @@ v | [ arg ] <- termToStr <$> (take 1 . V.toList) v = f (read arg)
 
 instance ( Ground r ~ 'True
          , Read a, Read b
          ) => Applicable (a -> b -> r) where
-  f @@ v | [ arg1, arg2 ] <- symToStr <$> V.toList v =
+  f @@ v | [ arg1, arg2 ] <- termToStr <$> (take 2 . V.toList) v =
     f (read arg1)
       (read arg2)
 
 instance ( Ground r ~ 'True
          , Read a, Read b, Read c
          ) => Applicable (a -> b -> c -> r) where
-  f @@ v | [ arg1, arg2, arg3 ] <- symToStr <$> V.toList v =
+  f @@ v | [ arg1, arg2, arg3 ] <- termToStr <$> (take 3 . V.toList) v =
     f (read arg1)
       (read arg2)
       (read arg3)
@@ -142,7 +181,7 @@ instance ( Ground r ~ 'True
 instance ( Ground r ~ 'True
          , Read a, Read b, Read c, Read d
          ) => Applicable (a -> b -> c -> d -> r) where
-  f @@ v | [ arg1, arg2, arg3, arg4 ] <- symToStr <$> V.toList v =
+  f @@ v | [ arg1, arg2, arg3, arg4 ] <- termToStr <$> (take 4 . V.toList) v =
     f (read arg1)
       (read arg2)
       (read arg3)
@@ -151,18 +190,22 @@ instance ( Ground r ~ 'True
 instance ( Ground r ~ 'True
          , Read a, Read b, Read c, Read d, Read e
          ) => Applicable (a -> b -> c -> d -> e -> r) where
-  f @@ v | [ arg1, arg2, arg3, arg4, arg5 ] <- symToStr <$> V.toList v =
+  f @@ v | [ arg1, arg2, arg3, arg4, arg5 ] <- termToStr <$> (take 5 . V.toList) v =
     f (read arg1)
       (read arg2)
       (read arg3)
       (read arg4)
       (read arg5)
 
-toSym :: Show a => a -> Sym
-toSym = Sym . pack . show
+toSym :: BaseTy -> Sym
+toSym (TString str) = Sym . pack $ str
+toSym (TInt i) = Sym . pack . show $ i
+toSym (TChar c) = Sym . pack . show $ c
 
-symToStr :: Sym -> [ Char ]
-symToStr (Sym text) = unpack text
+termToStr :: Term -> [ Char ]
+termToStr (TSym (Sym text)) = unpack text
+termToStr TVar{} = panic
+  "Mode error: Foreign function argument is not sufficiently bound."
 
 fromTerm :: Term -> Sym
 fromTerm = \case
