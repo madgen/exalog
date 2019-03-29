242208a4ccb38e388eee2d186156c5c859c10790
Mistral Contrastin <me@madgen.net>
2019-03-29 15:13:35 +0000

    Add some documentation about range restriction

--- src/Language/Exalog/RangeRestriction.hs
+++ src/Language/Exalog/RangeRestriction.hs
@@ -13,6 +13,19 @@ import Language.Exalog.Logger
 import Language.Exalog.Pretty
 import Language.Exalog.SrcLoc (span)
 
+-- |Checks if all variables in the head appear in the bodies of the
+-- clauses.
+--
+-- This allows domain independence (changing the underlying types in a way
+-- compatible with the data in the database/EDB) does not cause a change in
+-- the query results.
+--
+-- It also contributes to finiteness of results (allowing tabulation of all
+-- ground facts) by preventing concluding facts such as $p(X,X)$.
+--
+-- A final benefit is facilitating data provenance. This restriction does
+-- not solely ensure but contribute to the fact that all ground terms we
+-- use come from somewhere in the original EDB.
 checkRangeRestriction :: SpannableAnn (ClauseAnn ann) => Program ann -> Logger ()
 checkRangeRestriction Program{..} = traverse_ checkClause clauses
 
