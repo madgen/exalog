0d834b889e4d723bc240cab04cbebf3fb0559fbb
Mistral Contrastin <me@madgen.net>
2019-04-21 01:33:00 +0100

    Separate well-modability and well-modedness

--- src/Language/Exalog/WellModing.hs
+++ src/Language/Exalog/WellModing.hs
@@ -2,13 +2,16 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Language.Exalog.WellModing
-  ( WellModable(..)
+  ( WellModed(..)
+  , Moded(..)
   ) where
 
 import Protolude hiding (head)
 
+import           Data.Singletons (fromSing)
 import qualified Data.Vector.Sized as V
 
 import Language.Exalog.Adornment
@@ -16,25 +19,44 @@ import Language.Exalog.SrcLoc (span)
 import Language.Exalog.Core
 import Language.Exalog.Logger
 
-class WellModable ast where
-  checkWellModed :: ast -> Logger ()
-  isWellModed    :: ast -> Bool
+class WellModed ast where
+  checkWellModedness :: ast -> Logger ()
+  isWellModed        :: ast -> Bool
+
+instance ( SpannableAnn (LiteralAnn ann)
+         , Moded (Program ('AAdornment ann))
+         ) => WellModed (Program ann) where
+  checkWellModedness Program{..} = traverse_ checkWellModedness clauses
+  isWellModed        Program{..} = all isWellModed clauses
+
+instance ( SpannableAnn (LiteralAnn ann)
+         , Moded (Clause ('AAdornment ann))
+         ) => WellModed (Clause ann) where
+  checkWellModedness cl@Clause{..} = checkWellModability $ adornClause (allFree head) cl
+  isWellModed        cl@Clause{..} = isWellModable       $ adornClause (allFree head) cl
+
+allFree :: Literal ann -> [ Adornment ]
+allFree Literal{predicate = Predicate{..}} =
+  replicate (fromIntegral . fromSing $ arity) Free
+
+class Moded ast where
+  checkWellModability :: ast -> Logger ()
+  isWellModable       :: ast -> Bool
 
 instance SpannableAnn (LiteralAnn ann)
-    => WellModable (Program ('AAdornment ann)) where
-  checkWellModed Program{..} = traverse_ checkWellModed clauses
-  isWellModed    Program{..} = all isWellModed clauses
+    => Moded (Program ('AAdornment ann)) where
+  checkWellModability Program{..} = traverse_ checkWellModability clauses
+  isWellModable       Program{..} = all isWellModable clauses
+
+instance SpannableAnn (LiteralAnn ann) => Moded (Clause ('AAdornment ann)) where
+  checkWellModability Clause{..} = traverse_ checkWellModability body
+  isWellModable       Clause{..} = all isWellModable body
 
 instance SpannableAnn (LiteralAnn ann)
-    => WellModable (Clause ('AAdornment ann)) where
-  checkWellModed Clause{..} = traverse_ checkWellModed body
-  isWellModed    Clause{..} = all isWellModed body
-
-instance SpannableAnn (LiteralAnn ann)
-    => WellModable (Literal ('AAdornment ann)) where
-  checkWellModed lit =
-    when (isWellModed lit) $ scold (Just $ span lit) "Not well-moded."
-  isWellModed lit@Literal{..}
+    => Moded (Literal ('AAdornment ann)) where
+  checkWellModability lit =
+    when (isWellModable lit) $ scold (Just $ span lit) "Not well-modable."
+  isWellModable lit@Literal{..}
     | Positive <- polarity = True
     | otherwise = (`all` zip (adornment lit) (V.toList terms)) $ \case
       (Free,TVar{}) -> False
