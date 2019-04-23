357deb1cd1f5f139345b9bfaefac8af6e219d533
Mistral Contrastin <me@madgen.net>
2019-04-23 23:27:33 +0100

    Add range restriction checking predicate without IO

--- src/Language/Exalog/RangeRestriction.hs
+++ src/Language/Exalog/RangeRestriction.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-module Language.Exalog.RangeRestriction (checkRangeRestriction) where
+module Language.Exalog.RangeRestriction (RangeRestriction(..)) where
 
 import Protolude hiding (diff, head)
 
@@ -11,7 +12,7 @@ import qualified Data.List.NonEmpty as NE
 import Language.Exalog.Core
 import Language.Exalog.Logger
 import Language.Exalog.Pretty
-import Language.Exalog.SrcLoc (span)
+import Language.Exalog.SrcLoc (Spannable(..))
 
 -- |Checks if all variables in the head appear in the bodies of the
 -- clauses.
@@ -26,15 +27,18 @@ import Language.Exalog.SrcLoc (span)
 -- A final benefit is facilitating data provenance. This restriction does
 -- not solely ensure but contribute to the fact that all ground terms we
 -- use come from somewhere in the original EDB.
-checkRangeRestriction :: SpannableAnn (ClauseAnn ann) => Program ann -> Logger ()
-checkRangeRestriction Program{..} = traverse_ checkClause clauses
+class RangeRestriction ast where
+  checkRangeRestriction :: ast -> Logger ()
+  isRangeRestricted     :: ast -> Bool
 
-checkClause :: SpannableAnn (ClauseAnn ann) => Clause ann -> Logger ()
-checkClause cl@Clause{..} =
-  case diff of
-    []        -> pure ()
-    (var : _) -> scold (Just $ span cl) $
-      "Range restriction is violated " <> pp var <>
-      " doesn't occur in the body."
-  where
-  diff = variables head \\ mconcat (variables <$> NE.toList body)
+instance SpannableAnn (ClauseAnn ann) => RangeRestriction (Program ann) where
+  checkRangeRestriction Program{..} = traverse_ checkRangeRestriction clauses
+  isRangeRestricted Program{..} = all isRangeRestricted clauses
+
+instance SpannableAnn (ClauseAnn ann) => RangeRestriction (Clause ann) where
+  checkRangeRestriction cl =
+    unless (isRangeRestricted cl) $
+      scold (Just $ span cl) $ "Range restriction is violated."
+
+  isRangeRestricted Clause{..} =
+    null $ variables head \\ mconcat (variables <$> NE.toList body)
