26cfb24c3ee9c480a264de98b7e545c217cba365
Mistral Contrastin <me@madgen.net>
2019-11-20 00:01:07 +0000

    Expand data store interface

--- src/Language/Exalog/KnowledgeBase/Class.hs
+++ src/Language/Exalog/KnowledgeBase/Class.hs
@@ -1,27 +1,39 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Language.Exalog.KnowledgeBase.Class
   ( Knowledgeable(..)
+  , map
   ) where
 
-import Protolude
+import qualified Protolude as P
+import           Protolude hiding (pred, filter, toList, map)
 
-import Language.Exalog.Annotation
+import qualified Data.Vector.Sized as V
+
+import Language.Exalog.Core
+
 import Language.Exalog.KnowledgeBase.Knowledge
 
-class (Monoid (sol a)) => Knowledgeable sol a where
+class Knowledgeable sol a where
   fromList :: [ Knowledge a ] -> sol a
+  toList :: sol a -> [ Knowledge a ]
 
   add :: Knowledge a -> sol a -> sol a
+  partition :: (Knowledge a -> Bool) -> sol a -> (sol a, sol a)
   filter :: (Knowledge a -> Bool) -> sol a -> sol a
+  difference :: sol a -> sol a -> sol a
+  findByPred :: Predicate n a -> sol a -> [ V.Vector n Sym ]
 
-  atEach :: ( IdentifiableAnn (PredicateAnn b) id'
-            , Ord id'
-            ) => (Knowledge a -> Knowledge b) -> sol a -> sol b
+  atEach :: IdentifiableAnn (PredicateAnn b) id
+         => Ord id
+         => (Knowledge a -> Knowledge b) -> sol a -> sol b
 
-  empty :: sol a
   singleton :: Knowledge a -> sol a
 
   size :: sol a -> Int
   null :: sol a -> Bool
+
+map :: Knowledgeable kb a => (Knowledge a -> b) -> kb a -> [ b ]
+map f = P.map f . toList
--- src/Language/Exalog/KnowledgeBase/Set.hs
+++ src/Language/Exalog/KnowledgeBase/Set.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -8,34 +9,49 @@
 
 module Language.Exalog.KnowledgeBase.Set where
 
-import Protolude hiding (Set)
+import Protolude hiding (Set, toList, pred)
 
 import GHC.Prim (coerce)
 
 import qualified Data.Set as S
+import           Data.Singletons.Decide (Decision(Proved))
+import           Data.Type.Equality ((:~:)(Refl))
 
-import Language.Exalog.Annotation
+import Language.Exalog.Core
 import Language.Exalog.KnowledgeBase.Class
 import Language.Exalog.KnowledgeBase.Knowledge
 
 newtype Set ann = Set (S.Set (Knowledge ann))
 
-deriving instance (IdentifiableAnn (PredicateAnn ann) id, Eq id) => Eq (Set ann)
-deriving instance (IdentifiableAnn (PredicateAnn ann) id, Ord id) => Semigroup (Set ann)
-deriving instance (IdentifiableAnn (PredicateAnn ann) id, Ord id) => Monoid (Set ann)
+deriving instance Identifiable (PredicateAnn ann) id => Eq (Set ann)
+deriving instance Identifiable (PredicateAnn ann) id => Semigroup (Set ann)
+deriving instance Identifiable (PredicateAnn ann) id => Monoid (Set ann)
 
-instance ( IdentifiableAnn (PredicateAnn ann) id
-         , Ord id
-         ) => Knowledgeable Set ann where
+deriving instance Show (PredicateAnn ann) => Show (Set ann)
+
+instance (Identifiable (PredicateAnn ann) id) => Knowledgeable Set ann where
   fromList = Set . S.fromList
+  toList = S.toList . coerce
 
   add x = coerce . S.insert x . coerce
+
+  partition p = coerce . S.partition p . coerce
+
   filter p = coerce . S.filter p . coerce
+
+  difference kb kb' = coerce $ S.difference (coerce kb) (coerce kb')
+
+  findByPred pred kb = foldr' go mempty (toList kb)
+    where
+    go (Knowledge pred' syms) acc
+      | Proved Refl <- pred `sameArity` pred'
+      , pred == pred' = syms : acc
+      | otherwise = acc
 
   atEach f = coerce . S.map f . coerce
 
-  empty = coerce S.empty
   singleton = coerce . S.singleton
 
   size = S.size . coerce
+
   null = S.null . coerce
